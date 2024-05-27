7f8cc9af338d65d58e8ca8a161f572af23d63e08
Shiina Kin <shiina@sakurasou.io>
2024-05-27 20:54:27 +0800

    chore: v1.0.1
    perf: chooseSimilarSize logic

--- src/main/kotlin/io/sakurasou/service/ImageService.kt
+++ src/main/kotlin/io/sakurasou/service/ImageService.kt
@@ -189,34 +189,30 @@ class ImageService(
         queryConditionMap: Map<String, Any?>,
         queryCondition: String
     ): String {
-        var url = imageDTO.authority + "/"
+        val baseUrl = imageDTO.authority + "/"
         val th = queryConditionMap["th"]?.toString()?.toInt()
-        url += if (th != null) {
-            val i1 = (th - imageDTO.originalWidth).absoluteValue
-            val i2 = (th - ImageSize.W_1920.width).absoluteValue
-            val i3 = (th - ImageSize.W_1440.width).absoluteValue
-            val i4 = (th - ImageSize.W_1280.width).absoluteValue
-            val i5 = (th - ImageSize.W_960.width).absoluteValue
-            val i6 = (th - ImageSize.W_640.width).absoluteValue
-            val i7 = (th - ImageSize.W_320.width).absoluteValue
-            if (i2 == 0) imageDTO.w1920Path
-            else if (i3 == 0) imageDTO.w1440Path
-            else if (i4 == 0) imageDTO.w1280Path
-            else if (i5 == 0) imageDTO.w960Path
-            else if (i6 == 0) imageDTO.w640Path
-            else if (i7 == 0) imageDTO.w320Path
-            else when {
-                i1 < i2 && i1 < i3 && i1 < i4 && i1 < i5 && i1 < i6 && i1 < i7 -> imageDTO.originalPath
-                i2 < i1 && i2 < i3 && i2 < i4 && i2 < i5 && i2 < i6 && i2 < i7 -> imageDTO.w1920Path
-                i3 < i1 && i3 < i2 && i3 < i4 && i3 < i5 && i3 < i6 && i3 < i7 -> imageDTO.w1440Path
-                i4 < i1 && i4 < i2 && i4 < i3 && i4 < i5 && i4 < i6 && i4 < i7 -> imageDTO.w1280Path
-                i5 < i1 && i5 < i2 && i5 < i3 && i5 < i4 && i5 < i6 && i5 < i7 -> imageDTO.w960Path
-                i6 < i1 && i6 < i2 && i6 < i3 && i6 < i4 && i6 < i5 && i6 < i7 -> imageDTO.w640Path
-                i7 < i1 && i7 < i2 && i7 < i3 && i7 < i4 && i7 < i5 && i7 < i6 -> imageDTO.w320Path
-                else -> imageDTO.w1280Path
-            } + "?$queryCondition"
-        } else imageDTO.w1280Path
-        return url
+
+        fun buildUrl(path: String, includeQuery: Boolean): String {
+            val idQueryParam = "id=${imageDTO.id}"
+            val additionalQueryParams = if (includeQuery && queryConditionMap.isNotEmpty()) "&$queryCondition" else ""
+            return "$path?$idQueryParam$additionalQueryParams"
+        }
+
+        return baseUrl +
+                if (th == null) buildUrl(imageDTO.w1280Path, false)
+                else {
+                    val (width, path) = mapOf(
+                        imageDTO.originalWidth to imageDTO.originalPath,
+                        ImageSize.W_1920.width to imageDTO.w1920Path,
+                        ImageSize.W_1440.width to imageDTO.w1440Path,
+                        ImageSize.W_1280.width to imageDTO.w1280Path,
+                        ImageSize.W_960.width to imageDTO.w960Path,
+                        ImageSize.W_640.width to imageDTO.w640Path,
+                        ImageSize.W_320.width to imageDTO.w320Path
+                    ).minBy { (width, _) -> (th - width).absoluteValue }
+
+                    buildUrl(path, width != th)
+                }
     }
 
 }
